9e865ac0ad469b542732832c174dfb388b75e468
nerdgamer2662 <32596078+nerdgamer2662@users.noreply.github.com>
2022-10-22 15:16:27 -0400

    Input completed, format finalized (more or less)

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -5,19 +5,38 @@ import './App.css';
 function App() {
   const [file, changeFile] = useState();
   const [isFile, switchFile] = useState(false);
+  const [x, changeX] = useState("");
+  const [y, changeY] = useState("");
 
   return(    
-    <div class="App">
-      <p>Upload CSV:</p>
+    <div className="App">
+    <div className="horizontal">
+      <label htmlFor="file">Upload CSV:</label>
       <input type="file" name="file" onChange={(event) => {
         switchFile(true);
         changeFile(event.target.files[0]);
+        // send file? make temporary file htmlFor processing?
         }} />
+      <label htmlFor="type-selector">Choose a graph type:</label>
+      <select id="type-selector">
+        <option value="lin">Linear</option>
+        <option value="sc">Scatter</option>
+      </select>
+        </div>
+    <div className="horizontal">
+      <label htmlFor="x-axis">Horizontal:</label>
+      <input type="text" name="x-axis" onChange={(event) => {changeX(event.target.value)}}/>
+      <label htmlFor="y-axis">Vertical:</label>
+      <input type="text" name="y-axis" onChange={(event) => {changeY(event.target.value)}}/>
+    </div>
 
-        {isFile && <>
-        <p>File uploaded!</p>
-        </>
-        }
+    <div className="horizontal">
+    <button onClick={() => {console.log("foo")}}>Press me!</button>
+    </div>
+      {isFile && <>
+      <p>File uploaded!</p>
+      </>
+      }
      </div>
    )
 }
